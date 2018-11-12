9baf6b25793ba6b8471bf347dc5f28095a14ebd0
18031J0080 <44961082+18031J0080@users.noreply.github.com>
2018-11-13 00:13:32 +0530

    adsII

--- Solution.java
+++ Solution.java
@@ -5,26 +5,27 @@ import java.io.InputStreamReader;
 public class Solution {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
+	
      BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 	 String s=br.readLine();
-	// System.out.println("list "+s);
+	 
 		 switch(s)
 	 {
 	 case "List":
 		
-		 /*int ver=Integer.parseInt(br.readLine());
-		 System.out.println("v"+ver);
-		 int edg=Integer.parseInt(br.readLine());
-		 System.out.println("e"+edg);*/
-		 Graph g=new Graph(br);
+		 Graphs g=new Graphs(br);
          System.out.println(g.toString());
 		
 		 break;
 	 case "Matrix":
-
+		 int s1=Integer.parseInt(br.readLine());
+		 
+		 int s2=Integer.parseInt(br.readLine());
+		
+		 Matrix m1=new Matrix(br,s1,s2);
+		 System.out.println(s1+" vertices, "+s2+" edges");
+m1.display(s1, s2);
 		 break; 
 	 }
 	}
-
 }
