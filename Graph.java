a62e4117a28563fa36d6c2340197933d76bebe1b
18031J0080 <44961082+18031J0080@users.noreply.github.com>
2018-11-13 21:53:48 +0530

    ADSII80module2

--- Graph.java
+++ Graph.java
@@ -4,16 +4,19 @@ import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
 
 public class Graph {
-	private static final Object NEWLINE = null;
 	int V;
-	int E=0;
+	int E;
 	String b[];
      Bag<Integer>[] adj;
-     public Graph(int V)
+     CC cc;
+     
+     @SuppressWarnings("unchecked")
+	public Graph(int V)
      {
     	 this.V=V;
-    	 adj=(Bag<Integer>[])new Bag[V];
-    	 for(int v=0;v<V;v++)
+ 
+    	 adj=(Bag<Integer>[])new Bag[V+1];
+    	 for(int v=1;v<=V;v++)
     		 adj[v]=new Bag<Integer>();
      }
  int V()
@@ -31,64 +34,32 @@ Iterable<Integer> adj(int v)
 public Graph(BufferedReader in) throws IOException
 {
 this(Integer.parseInt(in.readLine()));
-int E=Integer.parseInt(in.readLine());
-if(E==0)
+String z=in.readLine();
+if(z==null)
 {
-	return;
+	 System.out.println("false");
+	 System.exit(0);;
 }
-String a=in.readLine();
-StringTokenizer st=new StringTokenizer(a,",");
- b=new String[st.countTokens()];
-int j=0;
-while(st.hasMoreTokens())
+while(z!=null)
 {
-	b[j++]=st.nextToken();
-	//System.out.println(b[0]+","+b[1]);
-}
-for(int i=0;i<E;i++)
-{
-	String z=in.readLine();
+	if(z.equals(""))
+	{
+		
+		return;
+	}
+	//System.out.println(z);
 	String r[]=z.split(" ");
 	int v=Integer.parseInt(r[0]);
 	int w=Integer.parseInt(r[1]);
 	addEddge(v,w);
+	z=in.readLine();
 }
 }
 void addEddge(int v,int w)
 {
-	if(!(v==w)||!duplicate(v,w))
+	//System.out.println(E);
 	E++;
 	adj[v].add(w);
 	adj[w].add(v);
-}
-private boolean duplicate(int v2,int w) {
-	 for (int w1 : adj[v2]) {
-	        if(w1==w)
-	        {
-	        	return true;
-	        }
-        }
-	return false;
-}
-public String toString() {
-    StringBuilder s = new StringBuilder();
-    s.append(V + " vertices, " + E + " edges "+"\n");
-    if(V==0||E==0)
-    {
-    	System.out.println("No edges");
-    }
-    for (int v = 0; v < V; v++) {
-    	
-        s.append(b[v] + ": ");
-        //System.out.println(v);
-        for (int w : adj[v]) {
-        
-       
-         s.append(b[w]+" ");
-        }
-        if(v!=V-1)
-        s.append("\n");
-    }
-    return s.toString();
 }
 }
--- Solution.java
+++ Solution.java
@@ -5,27 +5,12 @@ import java.io.InputStreamReader;
 public class Solution {
 
 	public static void main(String[] args) throws IOException {
-	
-     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-	 String s=br.readLine();
-	 
-		 switch(s)
-	 {
-	 case "List":
-		
-		 Graphs g=new Graphs(br);
-         System.out.println(g.toString());
-		
-		 break;
-	 case "Matrix":
-		 int s1=Integer.parseInt(br.readLine());
-		 
-		 int s2=Integer.parseInt(br.readLine());
-		
-		 Matrix m1=new Matrix(br,s1,s2);
-		 System.out.println(s1+" vertices, "+s2+" edges");
-m1.display(s1, s2);
-		 break; 
-	 }
+		// TODO Auto-generated method stub
+      BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+      Graph g=new Graph(br);
+      
+      CC c=new CC(g);
+	 c.display();
 	}
+
 }
